466af1e498ff566d5854e5d3035b549ebcde017d
Zeeshan Ali Pirzada <zeeshan@lnxsrv03.seas.ucla.edu>
2014-04-03 15:25:34 -0700

    make_stream

--- read-command.c
+++ read-command.c
@@ -23,12 +23,19 @@ command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
 		     void *get_next_byte_argument)
 {
-  /* FIXME: Replace this with your implementation.  You may need to
-     add auxiliary functions and otherwise modify the source code.
-     You can also use external functions defined in the GNU C Library.  */
-  error (1, 0, "command reading not yet implemented");
-  return 0;
+    
+    void* arg = get_next_byte_argument;
+	command_stream_t stream;
+	stream->num = 0;                                                                                           
+	int c;
+	while ( (c = get_next_byte(arg) != EOF )
+	{
+        //construct stream here.
+	}
+
+	return stream;
 }
+
 
 command_t
 read_command_stream (command_stream_t s)

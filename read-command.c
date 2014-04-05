568b7a44bcc789a2fbb20309fb943ace88015fc4
Zeeshan Ali Pirzada <zeeshan@lnxsrv02.seas.ucla.edu>
2014-04-05 16:04:43 -0700

    generalized stack, added tokenizer stuff

--- read-command.c
+++ read-command.c
@@ -5,98 +5,84 @@
 #include "alloc.h"
 
 #include <error.h>
+#include <stdlib.h>
+#include <string.h>
 
 /* command stream */
 
-struct command_tree;
-{
-	command_t head;
-};
+
 
 struct command_stream
 {
-	command_tree* command_list;
-	int num;		
+	command_t head;			
 };
 
 /** Stack definitions
- *  TODO: Test, garbage collection **/
+ *                       */
 
+typedef struct stack* stack_t;
 
-struct command_stack
+struct stack
 {
-	command_t stack;    
+	void** data;
+	int max;
 	int top;
 };
 
-enum op_priority
+stack_t stack_init()
 {
-	FOUR;
-	THREE;
-	TWO;
-	ONE;
+	stack_t temp = (stack_t)checked_malloc(sizeof(struct stack));
+	temp->max = 32;
+	temp->top = 0;
+	temp->data = (void**)checked_malloc(temp->max * sizeof(void*));	
 }
 
-struct op
+void stack_free(stack_t s)
 {
-	enum op_priority priority;
-	char* op[3];
-};
-
-struct op_stack
-{
-	struct op* stack;	
-	int top;
-};
-
-
-void initStack(struct op_stack* op_stack)
-{
-	op_stack->stack = NULL;
-	op_stack->top = 0;
+	if (!s)
+		error(1,0, "cannot delete null stack");
+	free(s->data);
+	free(s);
 }
 
-void initStack(struct command_stack* cmd_stack)
+void push(stack_t s, void* data, size_t elemSize)
 {
-	cmd_stack->stack = NULL;
-	cmd_stack->top = 0;
+	if (!s)
+		error(1,0, "cannot push to null stack");
+	if (s->top == s->max)
+	{
+		s->max *= 2;
+		s->data = (void**)checked_realloc( s->data, s->max * sizeof(void*));
+	}
+	s->data[s->top] = (void*)checked_malloc(elemSize);
+	s->data[(s->top)++] = data;
 }
 
-char* pop(struct op_stack* op_stack)
+void* pop(stack_t s)
 {
-	char* op[3];
-	strcpy(op,op_stack->stack[top--].op);
-	return op;
+	if (!s || s->top == 0)
+		error(1,0, "cannot pop from null or empty stack");
+	return s->data[s->top--];
 }
 
-command_t pop(struct command_stack* cmd_stack)
+void* top(stack_t s)
 {
-	return cmd_stack->stack[top--];
+	if (!s)
+		error(1,0, "can't find top of null stack");
+	return s->data[s->top-1];
 }
 
-//pretty sure i did this wrong lol
-void push(struct op_stack* op_stack, struct op op)
+int stack_size(stack_t s)
 {
-	op_stack->top++;
-	op_stack->stack = (struct op*)checked_realloc( top * sizeof(struct op) );
-	strcpy(op_stack->stack[op_stack->top]->op,op);
+	return s->top;
 }
-
-//probably wrong as well
-void push(struct command_stack* cmd_stack, struct command cmd)
-{
-	cmd_stack->top++;
-	cmd_stack->stack = (command_t)checked_realloc( top * sizeof(struct command));
-	cmd_stack->stack[top] = cmd;
-}
-
 
 
 /* Auxillary Functions */
 
 int isValidChar(int c)
 {
-	return ( (c >= '0' && c <= '9') || ((c | 'a' - 'A') - 'a' < 26u) 
+	return ( (c >= '0' && c <= '9') || ((c | ('a' - 'A')) - 'a' < 26u) 
 				|| c == '!' || c == '@' || c == '%' 
 				|| c == '^' || c == '-' || c == '_'
 				|| c == '+' || c == ':' || c == ',' 
@@ -109,35 +95,87 @@ int isValidChar(int c)
 
 enum token_type
 {
-	SIMPLE;
-	PIPE;
-	SEMI_COLON;
-	NEWLINE;
-	SUBSHELL;
-	INVALID;
-	END_OF_FILE;
-	WHITESPACE;
-	AND;
-	OR;
-	LESS_THAN;
-	GREATER_THAN;
-	OPEN_PARA;
-	CLOSE_PARA;
-	BAD;
-	
-}
+	SIMPLE,
+	PIPE,
+	SEMI_COLON,
+	DOUBLE_NEWLINE,  //2 newlines seperates commands
+	NEWLINE,
+	SUBSHELL,
+	INVALID,
+	END_OF_FILE,
+	WHITESPACE,
+	AND,
+	OR,
+	LESS_THAN,
+	GREATER_THAN,
+	OPEN_PARA,
+	CLOSE_PARA,
+	EMPTY,
+	WORD,
+};
+
+typedef struct token* token_t;
 
 struct token
 {
-	char data*;
+	char* data;
 	enum token_type type;
-	struct token* next;
-	struct token* prev;
+	token_t next;
+	token_t prev;
 };
 
-enum token_type get_type(char* data);
+enum token_type get_type(char* data)
+{
+	int len = strlen(data);
 
-struct token* get_next_token(int* (get_next_byte) (void*), void* arg);
+	if (len == 0)
+		return EMPTY;	
+
+	int i = 0;
+	while (data[i] != '\0')
+	{
+		char c = data[i];
+		if (len == 1)
+		{
+			switch (c)
+			{
+				case ' ': return WHITESPACE;
+				case '\n': return NEWLINE;
+				case -1: return END_OF_FILE;
+				case ';': return SEMI_COLON;
+				case '(': return OPEN_PARA;
+				case ')': return CLOSE_PARA;
+				case '<': return LESS_THAN;
+				case '>': return GREATER_THAN;
+				case '|': return PIPE;
+				default: break;
+			}
+		}
+		if (len == 2)
+		{
+			if (data[i] == '&' && data[i+1] == '&')
+				return AND;
+			if (data[i] == '|' && data[i+1] == '|')
+				return OR; 
+			if (data[i] == '\n' && data[i+1] == '\n')
+				return DOUBLE_NEWLINE;
+		}
+					
+		if (isValidChar(data[i]))
+		{
+			i++;
+			continue;
+		}
+		else
+		{
+			return INVALID;
+		}
+		i++;
+	}
+	return WORD;
+}
+
+struct token* get_next_token(int* (get_c) (void*), void* arg);
 
 
 
@@ -148,18 +186,14 @@ make_command_stream (int (*get_next_byte) (void *),
     
 	
 
-	command_stream_t stream = (command_stream_t)checked_malloc(sizeof(struct command_stream)); 
-	
-	struct op_stack op_stack;
-	struct command_stack cmd_stack;
-	initStack(
-
-
+//basic idea: make linked list of tokens
+//remove any newlines
+//put into treees
 	int c;
 	while ((c = get_next_byte(get_next_byte_argument)) != -1)
 		continue;
-
-	return stream;
+	
+	return 0;
 }
 
 

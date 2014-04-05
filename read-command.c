aea9bf4b4d98140b1bfe71bbb0fb625586c35e8f
Zeeshan Ali Pirzada <zeeshan@lnxsrv02.seas.ucla.edu>
2014-04-04 21:25:09 -0700

    added some structs/functions

--- read-command.c
+++ read-command.c
@@ -6,25 +6,100 @@
 
 #include <error.h>
 
-typedef struct command_node* command_node_t;
+/* struct definitions */
 
-struct command_node 
+struct command_tree;
 {
-	command_t command;
-	command_node_t next;
+	command_t head;
 };
-
 
 struct command_stream
 {
-	command_node_t head;	
+	command_tree* command_list;
+	int num;		
 };
+
+struct command_stack
+{
+	command_t stack;    
+	int top;
+
+};
+
+struct op_stack
+{
+	enum command_type* stack;
+	int top;
+};
+
+enum op_priority
+{
+	FOUR;
+	THREE;
+	TWO;
+	ONE;
+};
+
+
+/* auxillary functions */
+
+
+void initStack(struct op_stack)
+{
+	op_stack.stack = NULL;
+	int top = 0;
+}
+
+void initStack(struct command_stack)
+{
+	command_stack.stack = NULL;
+	int top = 0;
+}
+
+char* pop(struct op_stack)
+{
+	char* op[3];
+	strcpy(op,op_stack.stack[top--]);
+	return op;
+}
+
+command_t pop(struct command_stack)
+{
+	return command_stack.stack[top--];
+}
+
+//pretty sure i did this wrong lol
+void push(struct op_stack, char* op)
+{
+	op_stack.stack = (struct command*)checked_realloc(sizeof(op_stack.stack)+sizeof(struct command*));
+	top++;
+	strcpy(op_stack.stack[top],op);
+}
+
+//will wait till i get the other one
+void push(struct command_stack, command_t cmd)
+{
+
+}
+
+int isValidChar(int c)
+{
+	return ( (c >= '0' && c <= '9') || ((c | 'a' - 'A') - 'a' < 26u) 
+				|| c == '!' || c == '@' || c == '%' 
+				|| c == '^' || c == '-' || c == '_'
+				|| c == '+' || c == ':' || c == ',' 
+				|| c == '.' || c == '/');
+}
+
+
 
 command_stream_t
 make_command_stream (int (*get_next_byte) (void *),
 		     void *get_next_byte_argument)
 {
     
+	//command_stream_t is an array tree's that represent each complete command
+
 	command_stream_t stream = (command_stream_t)checked_malloc(sizeof(struct command_stream)); 
 	stream->head = 0;
 	int c;

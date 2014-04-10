7ce9cc69123e0d13b1d6f5da82d45df3518b4998
Kevin Chihpei Sheu <sheu@lnxsrv02.seas.ucla.edu>
2014-04-10 15:40:57 -0700

    added zeeshan changes

--- read-command.c
+++ read-command.c
@@ -12,9 +12,17 @@
 #include <ctype.h>
  
  
+typedef struct command_node* command_node_t;
+ 
+struct command_node
+{
+	command_t data;
+	command_node_t next;
+}
+ 
 struct command_stream
 {
-	command_t head;			
+	command_node_t head;			
 };
  
  
@@ -77,18 +85,13 @@ int isValidChar(int c)
 			|| c == '!' || c == '@' || c == '%' || c == '^' || c == '-' || c == '_'
 			|| c == '+' || c == ':' || c == ',' || c == '.' || c == '/');
 }
- 
- 
 /*  Tokenizer stuff */
- 
  
 enum token_type
 {
 	PIPE,
 	SEMI_COLON,
 	NEWLINE,
-	INVALID,
-	END_OF_FILE,
 	AND,
 	OR,
 	LESS_THAN,
@@ -224,7 +227,6 @@ token_t create_token_list(char* file)
  
 }
  
- 
 char* readFile(int (*get_next_byte) (void *), void* get_next_byte_argument)
 {
 	int inputSize = 128;
@@ -246,25 +248,103 @@ char* readFile(int (*get_next_byte) (void *), void* get_next_byte_argument)
  
 }
  
+typedef struct op op_t;
+ 
+struct op
+{
+	char* data;
+	enum token_type type;
+};
+ 
+int precedence(op_t newOperator, op_t oldOperator)
+{
+	//return true if new > old
+	if (newOperator->type == PIPE)
+		return ( oldOperator->type == GREATER_THAN || oldOperator->type == LESS_THAN );
+ 
+	if (newOperator->type == GREATER_THAN || newOperator->type == LESS_THAN)
+		return (oldOperator->type != GREATER_THAN && oldOperator->type != LESS_THAN);
+ 
+	if (newOperator->type == AND || newOperator->type == OR)
+		return (oldOperator->type != AND || oldOperator->type != OR);
+ 
+}
+ 
+op_t allocate_operator(enum token_type type); //todo
+ 
+command_t make_command_t(token_t head)
+{
+	//initialize stacks.
+	stack_t cmd_stack = stack_init();
+	stack_t op_stack = stack_init();
+ 
+	//start parsing token list.
+	token_t curr = head->next;
+	int numLine = 0;
+	while (curr != 0)
+	{
+			
+		//we encounter a simple command.
+		if (curr->type == WORD )
+		{
+			//sequences of 1 or more words are valid for simple commands.
+			int len = strlen(curr->data) + 1;
+			command_t cmd;
+			cmd->type = SIMPLE_COMMAND;
+			cmd->input = 0;
+			cmd->output = 0;
+			cmd->word = checked_malloc(sizeof(char*));
+			int i = 0;
+			cmd->word[i] = checked_malloc(len * sizeof(char));
+			strcpy(cmd->word[i++],curr->data);
+			//get rest of command
+			curr = curr->next;
+			while ( curr != 0 && curr->type == WORD )
+			{
+				cmd->word = checked_realloc(cmd->word,(i+1)*sizeof(char*));
+				cmd->word[i] = checked_malloc((strlen(curr->data)+1) * sizeof(char));
+				strcpy(cmd->word[i++],curr->data);
+				curr = curr->next;
+			}
+			stack_push(cmd_stack,cmd);
+			continue;
+		}
+ 
+		if (curr->type == GREATER_THAN || curr->type == LESS_THAN || curr->type == AND || curr->type == OR || curr->type == PIPE)
+		{
+			op_t new_operator = allocate_operator(enum token_type type);
+			op_t top_operator = stack_top(op_stack);
+ 
+			if ( precedence(new_operator,top_operator))
+			{
+				stack_push(op_stack, new_operator);
+			}
+			else
+			{
+				while ( top_operator->type != OPEN_PARA && !precedence(new_operator,top_operator))
+				{
+					op_t oper = stack_pop(op_stack);
+					command_t 
+ 
+					if (stack_size(op_stack) == 0)
+						break;
+				}
+			}
+		}
+		
+		
+	}
+	stack_free(cmd_stack);
+	stack_free(op_stack);
+}
+ 
  
  
 command_stream_t make_command_stream (int (*get_next_byte) (void *), void *get_next_byte_argument)
 {
-	/* read in file */
 	char* file = readFile(get_next_byte, get_next_byte_argument);
 	token_t head = create_token_list(file);
-	token_t temp = head->next;
-	command_stream_t cs;
-	while ( temp != 0)
-	{
-		//create command tree's here.
- 
-	}
- 
-	return cs;
- 
- 
- 
+//	return make_command_stream_t(head);
 }
  
  
